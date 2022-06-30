4f762f146ebd3798f52f3e27a50089df4b7a5d98
MohamadEdris <medris2796@gmail.com>
2022-06-30 15:20:18 +0300

    Added new project files

---
+++ src/main/java/com/example/fursa/JenkinsClosingTask/JenkinsClosingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.fursa.JenkinsClosingTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsClosingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsClosingTaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/fursa/JenkinsClosingTask/controller.java
@@ -0,0 +1,25 @@
+package com.example.fursa.JenkinsClosingTask;
+
+import com.example.fursa.JenkinsClosingTask.Table;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+
+@RestController
+public class controller {
+
+    @RequestMapping
+    public ModelAndView table(Model model) throws IOException {
+        Table table = new Table("http://www.ynet.co.il/Integration/StoryRss2.xml");
+
+        model.addAttribute("content",table.getTable());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("table");
+        return modelAndView;
+    }
+}
